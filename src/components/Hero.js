924da453f573235783992da24990db1207b6aabf
Aayushman03 <aayushmanpratap333@gmail.com>
2023-08-14 00:39:39 +0530

    navbar changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import RightHeroComonent from './RightHeroComonent'
 const Hero = () => {
   return (
 
-    <div className='h-4/5 flex flex-col-reverse items-center justify-center lg:flex-row'>
+    <div className='h-4/5 flex flex-col-reverse items-center justify-center lg:flex-row bg-blue-500'>
        <LeftHeroComponent/>
        <RightHeroComonent/>
     </div>
